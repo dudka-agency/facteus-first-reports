353f22ec4886c9d6002df0ae37a7b0daf915fb97
nickreznichuk <nickreznichuk@gmail.com>
2021-09-28 12:48:32 +0300

    [*] ver 0.1

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,29 @@
+import os
+
 from django.contrib import admin
 
-from .models import FirstReport
+from django.template.loader import get_template
 
-admin.site.register(FirstReport)
+from .models import FirstReport
+import pdfkit
+
+
+class FirstReportAdminSite(admin.ModelAdmin):
+    actions = ['make_report']
+
+    def make_report(self, req, query):
+        for obj in query:
+            context = {
+                "invoice_id": 123,
+                "customer_name": "John Cooper",
+                "amount": 1399.99,
+                "today": "Today",
+                'merchant': obj.merchant
+            }
+            template = get_template('first-report.html')
+            html = template.render(context)
+            pdfkit.from_string(html, obj.merchant + '-out.pdf')
+
+
+
+admin.site.register(FirstReport, FirstReportAdminSite)
